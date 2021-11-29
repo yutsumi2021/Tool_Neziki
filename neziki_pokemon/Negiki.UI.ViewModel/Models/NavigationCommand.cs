21347d3b1dbaa698eadde0ca6359463c3ed39edd
yutsumi <miutu.me.am@gmail.com>
2021-11-29 21:22:48 +0900

    20211129

---
+++ neziki_pokemon/Negiki.UI.ViewModel/Models/NavigationCommand.cs
@@ -0,0 +1,100 @@
+﻿using Negiki.UI.function;
+using Negiki.UI.ViewModel;
+using Negiki.UI.Views;
+using System;
+using System.Windows;
+
+namespace Negiki.UI.ViewModel.Models
+{
+    public class NavigationCommand : INavigation
+    {
+        private static Window _CurrentWindow;
+        private static Window _StartupWindow;
+        private static Window _MainWindow;
+
+        public void OpenWindow(string key)
+        {
+            switch(key)
+            {
+                //起動画面
+                case nameof(StartupWindowViewModel):
+                    OpenStartupWindow();
+                    break;
+                //メイン画面
+                case nameof(MainWindowViewModel):
+                    MainWindowViewModel();
+                    break;
+                //計算画面
+
+                default:
+                    throw new ArgumentException($"想定外の{(key)}が指定されました。");
+            }
+        }
+
+        private void MainWindowViewModel()
+        {
+            _MainWindow = new MainWindow()
+            {
+                DataContext = new MainWindowViewModel()
+            };
+            _CurrentWindow = _MainWindow;
+            _MainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            _MainWindow.Show();
+            _MainWindow.Activate();
+            if (IsDisposed(_StartupWindow))
+            {
+                _StartupWindow.Close();
+            }
+        }
+
+        public void CloseWindow()
+        {
+            
+        }
+
+        public void ShowDialog(string key)
+        {
+            switch (key)
+            {
+                //持ち物
+
+                //技
+
+                //特性
+            }
+        }
+
+        public void ExitAppcation()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OpenStartupWindow()
+        {
+            var ViewModel = new StartupWindowViewModel();
+            _StartupWindow = new StartupWindow()
+            {
+                DataContext = ViewModel
+            };
+            _StartupWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            _StartupWindow.ContentRendered += (sender, e) => ViewModel.TaskRun();
+            _StartupWindow.Show();
+            _StartupWindow.Activate();
+        }
+
+        /// <summary>
+        /// 画面のリソースを破棄してbool値を返します。
+        /// </summary>
+        /// <param name="window"></param>
+        /// <returns></returns>
+        private bool IsDisposed(Window window)
+        {
+            if (window == null)
+                return true;
+
+            var source = PresentationSource.FromVisual(window);
+
+            return source == null || source.IsDisposed;
+        }
+    }
+}
---
+++ neziki_pokemon/Negiki.UI.ViewModel/Models/StartupWindowModel.cs
@@ -0,0 +1,54 @@
+﻿using Neziki.Domain.Services.Factory;
+using Neziki.Repository;
+using Neziki.Service;
+using Neziki.XmlData.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Negiki.UI.ViewModel.Models
+{
+    public class StartupWindowModel
+    {
+        int persent = 0;
+
+        public async void RunTask()
+        {
+
+        }
+
+        public bool DoWork(IProgress<int> progress)
+        {
+            try
+            {
+                Task.Run(() =>
+                {
+                    while (persent < 100)
+                    {
+                        LoadData();
+                        persent++;
+                        // 状況通知
+                        progress.Report(persent);
+                    }
+                });
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void LoadData()
+        {
+            //Xmlからデータ取得
+            Xml_List.InitializeAll();
+            //各データを生成
+            Data_ItemServiceFactory.Initialize(new Data_ItemService(new ItemRepository()));
+            Data_SkillServiceFactory.Initialize(new Data_SkillService(new SkillRepository()));
+            Data_SpecialServiceFactory.Initialize(new Data_SpecialService(new SpecialRepository()));
+            var syuzokutiRepository = new syuzokutiRepository();
+            Data_syuzokutiServiceFactory.Initialize(new Data_syuzokutiService(syuzokutiRepository));
+            Data_PokemonServiceFactory.Initialize(new Data_PokemonService(new PokemonRepository(syuzokutiRepository)));
+        }
+    }
+}
